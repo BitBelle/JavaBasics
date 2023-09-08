2a997d1e77e9db462d11bbfca1749ab85b51a4b5
BitBelle <wainainamercy432@gmail.com>
2023-09-08 12:43:48 +0300

    local variables in Java

---
+++ JavaBasics/src/localVariable/HelloApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package localVariable;
+
+/**
+ *
+ * @author capricorn
+ */
+public class HelloApp {
+    
+    
+    public static void main(String[]args){
+        
+        String helloMessage;
+        helloMessage = "Hello World";
+        System.out.println(helloMessage);
+    }
+    
+}
