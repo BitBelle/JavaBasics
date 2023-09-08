39bfd86dd956493e30506d5a5778258deff286ad
BitBelle <wainainamercy432@gmail.com>
2023-09-08 12:49:07 +0300

    types defined by classes in the Java API

---
+++ JavaBasics/src/referenceType/Greetings.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package referenceType;
+
+/**
+ *
+ * @author capricorn
+ */
+public class Greetings {
+
+     String B = "Greetings";
+     
+  
+     
+ 
+    public static void main(String[]args){
+        Greetings obj1 = new Greetings();
+        obj1.B = "Hello World" ;
+        System.out.println(obj1.B);
+     
+    }
+    
+}
